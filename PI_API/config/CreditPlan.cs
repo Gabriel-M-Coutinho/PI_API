79fe6781478412c397255ebe305359c62af8c708
gabriel-m-coutinho <gabrielshot1@gmail.com>
2025-11-27 00:11:10 -0300

    att plans

--- PI_API/config/CreditPlan.cs
+++ PI_API/config/CreditPlan.cs
@@ -7,8 +7,8 @@ public static class CreditPlans
     public static readonly Dictionary<CreditPlan, (double Price, int Credits)> Plans =
         new Dictionary<CreditPlan, (double, int)>
         {
-            { CreditPlan.Basic, (19.90, 900) },
-            { CreditPlan.Intermediario, (39.90, 2000) },
-            { CreditPlan.Plus, (79.90, 4300 ) }
+            { CreditPlan.Basic, (19.90, 800) },
+            { CreditPlan.Intermediario, (39.90, 1800) },
+            { CreditPlan.Plus, (79.90, 3700 ) }
         };
 }
